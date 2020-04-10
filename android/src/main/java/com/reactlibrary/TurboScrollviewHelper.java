9837a9494d4e0f1e6291f68c8ba56c460544fcb5
Rokas <kasinskas.r@gmail.com>
2020-04-10 14:26:49 +0300

    add helpers

---
+++ android/src/main/java/com/reactlibrary/TurboScrollviewHelper.java
@@ -0,0 +1,8 @@
+package com.reactlibrary;
+
+import com.facebook.react.views.scroll.ReactScrollViewHelper;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+
+public class TurboScrollviewHelper extends ReactScrollViewHelper {
+}

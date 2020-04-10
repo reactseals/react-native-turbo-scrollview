de218e57049cd48c2353e24873355cf2b48fb177
Rokas <kasinskas.r@gmail.com>
2020-04-10 14:25:49 +0300

    remove useless shit

--- android/src/main/java/com/reactlibrary/TurboScrollviewPackage.java
+++ android/src/main/java/com/reactlibrary/TurboScrollviewPackage.java
@@ -19,7 +19,6 @@ public class TurboScrollviewPackage implements ReactPackage {
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Arrays.<ViewManager>asList(new TurboScrollviewManager(), new TurboHorizontalScrollviewManager(),
-                new TurboHorizontalScrollContentViewManager(), new DirectedScrollViewManager(),
-                new DirectedScrollViewChildManager());
+                new TurboHorizontalScrollContentViewManager(), new DirectedScrollViewManager());
     }
 }
